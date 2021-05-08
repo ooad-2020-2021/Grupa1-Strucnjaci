e1d736fe3d7cd5e90489fc3a6d3a7de9313da66e
Anesa Eminagić <73439016+aeminagic1@users.noreply.github.com>
2021-05-08 18:27:08 +0200

    klasa Premium

---
+++ Aplikacija/SpaceCat/SpaceCat/Models/Premium.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceCat
+{
+    public class Premium : Macka
+    {
+        public string BankovniRacun { get; set; }
+    }
+}
