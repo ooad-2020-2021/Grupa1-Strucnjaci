2bdb369e495db6b4fb29199a8301b12073bc7307
bhadzic2 <bhadzic2@etf.unsa.ba>
2021-05-08 18:35:48 +0200

    implementirana klasa Novost

--- Aplikacija/SpaceCat/SpaceCat/Models/Novost.cs
+++ Aplikacija/SpaceCat/SpaceCat/Models/Novost.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
-public class Class1
+namespace SpaceCat
 {
-	public Class1()
-	{
+	public class Novost {
+		public Novost(string naslov, string text, DateTime vrijemeObjave, List<string> slike, List<Komentar> komentari)
+		{
+			Naslov = naslov;
+			Text = text;
+			Slike = slike;
+			VrijemeObjave = vrijemeObjave;
+			Komentari = komentari;
+		}
+		public int ID { get; }
+		public string Naslov { get; set; }
+		public string Text { get; set; }
+		public DateTime VrijemeObjave { get; }
+		public List<string> Slike { get; set; }
+		public List<Komentar> Komentari { get; set; }
 	}
 }
