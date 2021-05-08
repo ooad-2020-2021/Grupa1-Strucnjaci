ca208de0fc11e624a4ae3430035fceb89c3eb349
Anesa Eminagić <73439016+aeminagic1@users.noreply.github.com>
2021-05-08 19:04:32 +0200

    Putovanje klasa

--- Aplikacija/SpaceCat/SpaceCat/Models/Putovanje.cs
+++ Aplikacija/SpaceCat/SpaceCat/Models/Putovanje.cs
@@ -1,8 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-public class Class1
+namespace SpaceCat
 {
-	public Class1()
-	{
-	}
+    public class Putovanje
+    {
+        public int id { get; }
+        public List<Macka> SpisakMacaka { get; set; }
+        public DateTime VrijemePolaska { get; set; }
+        public Destinacija Destinacija { get; set; }
+        public DateTime KrajPutovanja { get; set; }
+        public Putovanje(DateTime vrijemePolaska, Destinacija destinacija, DateTime krajPuta)
+        {
+            VrijemePolaska = vrijemePolaska;
+            Destinacija = destinacija;
+            KrajPuta = krajPuta;
+        }
+        public void DodajMacku(Macka macka)
+        {
+            SpisakMacaka.Add(Macka);
+        }
+    }
 }
