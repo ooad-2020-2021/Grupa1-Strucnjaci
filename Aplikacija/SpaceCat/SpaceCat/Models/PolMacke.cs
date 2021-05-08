6c44ab60f173f98cc2be3b8e411181eb8e49f852
bhadzic2 <bhadzic2@etf.unsa.ba>
2021-05-09 01:46:52 +0200

    dodani displeji u enume

--- Aplikacija/SpaceCat/SpaceCat/Models/PolMacke.cs
+++ Aplikacija/SpaceCat/SpaceCat/Models/PolMacke.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace SpaceCat
 {
     public enum PolMacke
     {
+        [Display(Name = "Musko")]
         MUSKO,
+        [Display(Name = "Zensko")]
         ZENSKO
     }
 
