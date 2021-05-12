d5ef66d801baf786c3d9fe6b266ab80aa1d288dd
bhadzic2 <bhadzic2@etf.unsa.ba>
2021-05-12 12:31:04 +0200

    Provjera baze zavrsena

--- Aplikacija/SpaceCat/SpaceCat/Startup.cs
+++ Aplikacija/SpaceCat/SpaceCat/Startup.cs
@@ -27,13 +27,13 @@ namespace SpaceCat
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<ApplicationDbContext>(options =>
-            //    options.UseSqlServer(
-            //        Configuration.GetConnectionString("DefaultConnection")));
-            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            //    .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseMySQL(
+                    Configuration.GetConnectionString("SpaceCatContext")));
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
-            //services.AddRazorPages();
+            services.AddRazorPages();
 
             services.AddDbContext<SpaceCatContext>(options =>
                     options.UseMySQL(Configuration.GetConnectionString("SpaceCatContext")));
