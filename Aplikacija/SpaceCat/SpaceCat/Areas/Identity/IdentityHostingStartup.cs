f03f4724943451576c90bdf89b122cf33a785d45
bhadzic2 <bhadzic2@etf.unsa.ba>
2021-06-13 01:14:06 +0200

    zabranjene neke stvari korisniku

---
+++ Aplikacija/SpaceCat/SpaceCat/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SpaceCat.Data;
+
+[assembly: HostingStartup(typeof(SpaceCat.Areas.Identity.IdentityHostingStartup))]
+namespace SpaceCat.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
